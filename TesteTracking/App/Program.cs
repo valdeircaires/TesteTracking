9aa6eb1169e36fd19d9a9a5459517903d732c7ac
Valdeir Silva Caires <valdeircaires@hotmail.com>
2019-05-10 09:26:21 -0300

    resolvendo problema de trackin com dispose no contexto

--- TesteTracking/App/Program.cs
+++ TesteTracking/App/Program.cs
@@ -12,6 +12,53 @@ namespace TesteTracking.App
     {
         static void Main(string[] args)
         {
+            ExecutandoSemErro();
+            ExecutandoComErro();
+        }
+
+        static void ExecutandoSemErro()
+        {
+            var contexto = new ContextTeste();
+            var uow = new UnityOfWork(contexto);
+            var repo = new ProdutoGrupoRepository(contexto);
+            var service = new ProdutoGrupoService(repo, uow);
+
+            var id = Guid.NewGuid();
+
+            service.Inserir(new ProdutoGrupoCommand(id, "TESTE"));
+            contexto.Dispose();
+            contexto = new ContextTeste();
+            uow = new UnityOfWork(contexto);
+            repo = new ProdutoGrupoRepository(contexto);
+            service = new ProdutoGrupoService(repo, uow);
+
+            service.Atualizar(new ProdutoGrupoCommand(id, "TESTE DE ALTERACAO"));
+
+            contexto.Dispose();
+            contexto = new ContextTeste();
+            uow = new UnityOfWork(contexto);
+            repo = new ProdutoGrupoRepository(contexto);
+            service = new ProdutoGrupoService(repo, uow);
+
+            service.Atualizar(new ProdutoGrupoCommand(id, "TESTE DE ALTERACAO 2"));
+
+            contexto.Dispose();
+            contexto = new ContextTeste();
+            uow = new UnityOfWork(contexto);
+            repo = new ProdutoGrupoRepository(contexto);
+            service = new ProdutoGrupoService(repo, uow);
+
+            var grupo = service.ObterGrupoPorId(id);
+            if (grupo != null)
+                Console.WriteLine("NOME ATUAL DO GRUPO: " + grupo.NomeGrupo);
+
+            Console.Write("Aperte enter para continuar:");
+
+            Console.ReadLine();
+        }
+
+        static void ExecutandoComErro()
+        {
             var contexto = new ContextTeste();
             var uow = new UnityOfWork(contexto);
             var repo = new ProdutoGrupoRepository(contexto);
@@ -27,9 +74,11 @@ namespace TesteTracking.App
 
             var grupo = service.ObterGrupoPorId(id);
             if (grupo != null)
-                Console.WriteLine(grupo.NomeGrupo);
+                Console.WriteLine("NOME ATUAL DO GRUPO: " + grupo.NomeGrupo);
 
-            Console.ReadKey();
+            Console.Write("Aperte enter para continuar:");
+
+            Console.ReadLine();
         }
     }
 }
